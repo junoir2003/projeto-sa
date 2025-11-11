83cb414e86618dfc8fb4d52ea1521d3e9dad1d3b
ANDRÉ SILVA BARRETO JUNIOR <andre_barreto-junior@estudante.sesisenai.org.br>
2025-11-10 21:04:11 -0300

    mudou a pagina de livros

--- bookflow/pages/Livros/livros.js
+++ bookflow/pages/Livros/livros.js
@@ -1,12 +1,13 @@
 const livros = []
 
-function cadastrarLivro(){
+function cadastrarLivro(event) {
+    event.preventDefault()
     let nomeLivro = document.getElementById(`inputLivro`).value
     let autorLivro = document.getElementById(`inputAutor`).value
     let generoLivro = document.getElementById(`inputGenero`).value
     let paginasLivro = document.getElementById(`inputPaginas`).value
 
-    let novoLivro = {
+    const novoLivro = {
         nome: nomeLivro,
         autor: autorLivro,
         genero: generoLivro,
@@ -14,6 +15,33 @@ function cadastrarLivro(){
     }
 
     livros.push(novoLivro)
-   document.getElementById("cadastroOk").innerHTML = "O " + autorLivro + "é: " + generoLivro 
+    limpaLivro()
+    console.log(livros)
+    
+    mostrarOsLivros()
 }
-       
+
+function limpaLivro() {
+    document.getElementById(`inputLivro`).value = ''
+    document.getElementById(`inputAutor`).value = ''
+    document.getElementById(`inputGenero`).value = ''
+    document.getElementById(`inputPaginas`).value = ''
+
+    document.getElementById('inputLivro').focus()
+
+}
+
+function mostrarOsLivros() {
+    document.getElementById('listasLivros').innerHTML = ''
+    for (let i = 0; i < livros.length; i++) {
+        console.log(livros)
+        document.getElementById('listasLivros').innerHTML += `
+        <div class="card"> <h3> ${livros[i].nome}</h3> 
+        <p>Autor: ${livros[i].autor}</p>
+        <p>Autor: ${livros[i].genero}/<p>
+        <p>Autor: ${livros[i].paginas}</p>
+        </div>
+        `
+    }
+}
+
